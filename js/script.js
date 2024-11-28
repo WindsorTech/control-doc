37a9bd80d918b83adada0ce1525658f1fffe2541
Windsor Edeling <windsorjunior@hotmail.com>
2024-11-28 19:11:20 -0300

    updates

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ document.querySelector('form').addEventListener('submit', (e) => {
   alert('Obrigado por entrar em contato! Responderemos em breve.');
 });
 
-// script.js
+
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger");
   const menu = document.getElementById("menu");
 
-  // Toggle menu visibility
-  hamburger.addEventListener("click", () => {
-    menu.classList.toggle("show");
-  });
+  if (hamburger && menu) {
+    hamburger.addEventListener("click", () => {
+      menu.classList.toggle("show");
+    });
+  }
 });
